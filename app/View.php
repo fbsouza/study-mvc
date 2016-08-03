d6893f3d84810a42a3cf6705e9ffd51f3533d3fd
Fabio Souza <fsouza.me@gmail.com>
2016-08-03 15:41:08 -0300

    Add class view

---
+++ app/View.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+class View
+{
+	public static function make($viewName, array $customVars = array())
+	{
+		extract($customVars);
+
+		require_once viewsPath() . 'template.php';
+	}
+}
