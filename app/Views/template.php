20940032e2d03a6dd94cb8084ee0c19aff0d9367
Fabio Souza <fsouza.me@gmail.com>
2016-08-03 15:53:52 -0300

    Add generate template file

---
+++ app/Views/template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<?php
+		if (isset($viewName)) {
+			$path = viewspATH() . $viewName . '.php';
+
+			if (file_exists($path)) {
+				require_once $path;
+			}
+		}
+	?>
+</body>
+</html>
--- app/helpers.php
+++ app/helpers.php
@@ -14,15 +14,12 @@ public function viewsPath()
  */
 public function dateConvert($date)
 {
-    if ( ! strstr( $date, '/' ) )
-    {
+    if (!strstr( $date, '/' )) {
         // $date está no formato ISO (yyyy-mm-dd) e deve ser convertida
         // para dd/mm/yyyy
         sscanf($date, '%d-%d-%d', $y, $m, $d);
         return sprintf('%02d/%02d/%04d', $d, $m, $y);
-    }
-    else
-    {
+    } else {
         // $date está no formato brasileiro e deve ser convertida para ISO
         sscanf($date, '%d/%d/%d', $d, $m, $y);
         return sprintf('%04d-%02d-%02d', $y, $m, $d);
