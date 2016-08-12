78d6142309f5088ab880f9294d5b1bb45224809f
Fabio Souza <fsouza.me@gmail.com>
2016-08-12 12:08:39 -0300

    Add comment of tutorial

--- app/View.php
+++ app/View.php
@@ -4,6 +4,14 @@ namespace App;
 
 class View
 {
+	/**
+     * Exibe uma view.
+     * Função inspirada na usada pelo Laravel 4. Veja: http://laravel.com/docs/4.2/responses#views
+     *
+     * @link http://laravel.com/docs/4.2/responses#views
+     * @param  string $viewName   Nome da view, que é o nome do arquivo em lib/views, sem o ".php"
+     * @param  array  $customVars (opcional) Array com variáveis que serão usadas na view
+     */
 	public static function make($viewName, array $customVars = array())
 	{
 		extract($customVars);
